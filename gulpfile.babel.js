f08c26931010c161891d74ad1e7c3b3b22302284
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2015-11-18 11:27:58 +0900

    Update to archive (no more used)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,38 +7,51 @@ import {coffee, espower, mocha, react, rename, util} from './gulp_plugins.js';
 let paths = {
   components_dir: '../app/assets/javascripts/components/',
   test_dir: './test/',
+  module_sources: [
+    './node_modules/react-redux/dist/react-redux.min.js',
+    './node_modules/redux/dist/redux.min.js'
+  ],
+  modules_dir: '../app/assets/javascripts/modules/',
   transformed_components_dir: './transformed_components/',
-  powered_test_dir: './powered_test/',
+  powered_test_dir: './powered_test/'
 };
 
-gulp.task('default', ['test']);
+gulp.task('default', ['test:clean']);
 
-gulp.task('clean', () => {
-  del([paths.transformed_components_dir, paths.powered_test_dir])
+gulp.task('deploy_modules', () => {
+  gulp.src(paths.module_sources)
+    .pipe(gulp.dest(paths.modules_dir));
 });
 
-gulp.task('coffee_components', () => {
-  return gulp.src(paths.components_dir + '*.coffee')
+gulp.task('test:clean', () => {
+  del([paths.transformed_components_dir, paths.powered_test_dir]);
+});
+
+gulp.task('test:coffee_components', () => {
+  return gulp.src(paths.components_dir + '*.js.jsx.coffee')
     .pipe(coffee({bare: true}))
-    .pipe(rename({extname: '.jsx'}))
+    .pipe(rename({extname: ''}))
     .pipe(gulp.dest(paths.transformed_components_dir));
 });
 
-gulp.task('react_components', ['coffee_components'], () => {
-  return gulp.src(paths.transformed_components_dir + '*.jsx')
+gulp.task('test:react_components', ['test:coffee_components'], () => {
+  return gulp.src(paths.transformed_components_dir + '*.js.jsx')
     .pipe(react())
-    .pipe(rename({extname: '.js'}))
+    .pipe(rename({extname: ''}))
     .pipe(gulp.dest(paths.transformed_components_dir));
 });
 
-gulp.task('power_tests', ['react_components'], () => {
+gulp.task('test:power_tests', ['test:react_components'], () => {
   return gulp.src(paths.test_dir + '*_test.js')
     .pipe(react({es6module: true}))
     .pipe(espower())
     .pipe(gulp.dest(paths.powered_test_dir));
 });
 
-gulp.task('test', ['power_tests'], () => {
+gulp.task('test:prepare', ['test:power_tests'], () => {
+});
+
+gulp.task('test:run', ['test:prepare'], () => {
   gulp.src(paths.powered_test_dir + '*_test.js')
     .pipe(mocha().on('error', util.log));
 });
--- test/message_box_test.js
+++ test/message_box_test.js
@@ -5,13 +5,42 @@ import React from 'react/addons';
 
 let TestUtils = React.addons.TestUtils;
 
-//TODO: Change ugly filename
-import {MessageBox} from '../transformed_components/message_box.js.jsx.js';
+import {MessageBox} from '../transformed_components/message_box';
 
 describe('MessageBox', function () {
   describe('renderIntoDocument', function () {
-    it('should render the component', function () {
-      TestUtils.renderIntoDocument(<MessageBox url="http://example.com/messages"></MessageBox>);
+    var url
+    var userName;
+
+    before(function () {
+      url = 'http://example.com/messages';
+      userName = 'taro';
+    });
+
+    it('should have blank text content', function () {
+      var box = TestUtils.renderIntoDocument(<MessageBox url={url} userName={userName}></MessageBox>);
+      assert(React.findDOMNode(box).textContent === '');
+    });
+
+    context('with url', function () {
+      it('should have specified url in props', function () {
+        var box = TestUtils.renderIntoDocument(<MessageBox url={url}></MessageBox>);
+        assert(box.props.url === url);
+      });
+    });
+
+    context('without url', function () {
+      it('should not have url in props', function () {
+        var box = TestUtils.renderIntoDocument(<MessageBox></MessageBox>);
+        assert(box.props.url === undefined);
+      });
+    });
+
+    context('with userName', function () {
+      it('should have specified userName in props', function () {
+        var box = TestUtils.renderIntoDocument(<MessageBox url={url} userName={userName}></MessageBox>);
+        assert(box.props.userName === 'taro');
+      });
     });
   });
 });
